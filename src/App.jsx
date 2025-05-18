c1666327f99731f1023cc21b8d1a3669055fa8ce
Bahar <baharalekberova05gmail.com>
2025-05-18 19:37:35 +0400

    fetched products

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./layouts/Navbar";
-import Footer from "./layouts/Footer";
 import Home from "./components/Home";
 import { ContextProvider } from "./contexts/dataContext";
 
@@ -10,7 +9,6 @@ const App = () => {
       <div>
         <Navbar />
         <Home />
-        <Footer />
       </div>
     </ContextProvider>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,82 +1,60 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { DataContext } from "../contexts/dataContext";
 
 const Home = () => {
-  return (
-    <div className='container py-16 md:py-0'>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-    </div>
-  )
-}
+  const { data } = useContext(DataContext);
+  console.log(data);
 
-export default Home
+  return (
+    <div className="container py-16 md:py-4">
+      <p className="text-xl font-bold text-center md:text-left md:text-2xl">
+        Ətirlər
+      </p>
+      <p className="text-(--custom-gray) text-md text-center md:text-left md:text-md">
+        <span>0</span> nəticə
+      </p>
+
+      <div className="flex items-center space-x-2 my-4 md:hidden">
+        <button className="w-1/2 border border-(--custom-input-border) rounded-3xl px-6 py-1 text-sm font-semibold">
+          A-dan Z-yə
+        </button>
+
+        <button className="w- border border-(--custom-input-border) w-1/2 rounded-3xl px-6 py-1 text-sm cursor-pointer font-semibold">
+          Filter
+        </button>
+      </div>
+
+      <div className="flex space-x-6 mt-4">
+        <div className="flex-1 bg-fuchsia-400 hidden md:flex">Slmlar</div>
+
+        <div className="flex-3 grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8">
+          {data.map((parfume) => (
+            <div
+              key={parfume.id}
+              className="flex flex-col space-y-2 cursor-pointer group"
+            >
+              <div className="bg-white rounded-md h-[300px] py-8 group-hover:scale-110 transition-all duration-300 w-full relative">
+                <img
+                  src={parfume.img}
+                  alt={parfume.name}
+                  className="w-full h-full object-contain "
+                />
+
+                <button className="w-full rounded-full bg-white py-1 font-semibold cursor-pointer scale-y-0 group-hover:scale-y-100 origin-bottom transition-all duration-300 hover:bg-(--hover-color) absolute bottom-1">
+                  Səbətə əlavə et
+                </button>
+              </div>
+
+              <p className="text-xl font-bold">{parfume.price} AZN</p>
+              <p className="text-sm text-(--custom-black) font-medium">
+                {parfume.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
